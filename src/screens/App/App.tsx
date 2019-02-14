38b1f053e6cc8dd89d62e0d949aff7973ede95e8
diegojacomel <dmeloj@gmail.com>
2019-02-14 17:39:53 -0200

    first commit

---
+++ src/screens/App/App.tsx
@@ -0,0 +1,16 @@
+// Modules
+import React, { Component, Fragment } from "react";
+import { Route } from 'react-router-dom'; 
+import Home from '../Home/Home';
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Route exact path="/" component={Home}/>
+            </Fragment>
+        );
+    }
+}
+
+export default App;
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,12 @@
+// Modules
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home;
